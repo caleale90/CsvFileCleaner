8aa9fdbed11ee57f58ee64ab744f70d097eda5db
ArteLab Super Machine <bastapoco>
2017-10-11 12:38:10 +0200

    * commented the code * refactored

--- csvfilecleaner/__init__.py
+++ csvfilecleaner/__init__.py
@@ -1,15 +1,18 @@
+import argparse
 import os
 import re
 import sys
 from shutil import copyfile
 
 
-def write_to_file(file, text):
-    file_to_write = open(file, "a")
-    file_to_write.write(text + "\n")
-
-
 class CsvFileCleaner:
+    """
+    Constructor for the CsvFileCleaner object
+
+    file_to_process: path to the file that you want to clean
+    separator: character used as separator fro the CSV file
+    """
+
     def __init__(self, file_to_process, separator):
         self.file_to_process = file_to_process
         self.separator = separator
@@ -17,7 +20,14 @@ class CsvFileCleaner:
         if os.path.exists(self.tmp_file):
             os.remove(self.tmp_file)
 
+    """
+    Method to start cleaning the file
+    MUST be called after calling the constructor
+    """
+
     def process(self):
+        if self is None:
+            raise Exception("First you must instantiate an object of type CsvFileCleaner")
         file_to_process = open(self.file_to_process, 'r')
         for line in file_to_process.readlines():
             text_to_write = self.__clean(line)
@@ -27,17 +37,32 @@ class CsvFileCleaner:
     def __clean(self, text):
         result = ""
         for token in text.split(self.separator):
-            token = token.lower().replace("\n","")
+            token = token.lower().replace("\n", "")
             text = re.sub('[^0-9a-zA-Z]+', ' ', token)
             result += text + self.separator
         return result[:-1]
 
 
-def main():
-    file_to_process = sys.argv[1]
-    fc = CsvFileCleaner(file_to_process, '|')
+def write_to_file(file, text):
+    file_to_write = open(file, "a")
+    file_to_write.write(text + "\n")
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('file_to_process', type=str, help='Path of the CSV file that you want to clean')
+    parser.add_argument('--separator', type=str, help='Separator of the CSV file', default=",")
+
+    return parser.parse_args(argv)
+
+
+def main(args):
+    file_to_process = args.file_to_process
+    separator = args.separator
+    fc = CsvFileCleaner(file_to_process, separator)
     fc.process()
 
 
 if __name__ == '__main__':
-    main()
+    main(parse_arguments(sys.argv[1:]))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='csvfilecleaner',
-      version='0.1',
+      version='0.3',
       description='A very simple utility class for cleaning csv files',
       keywords='csv clean file',
       url='',
