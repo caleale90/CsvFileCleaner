d5db413a3a3adf45f2d45b70f912fae6a27769e3
ArteLab Super Machine <bastapoco>
2017-10-11 14:41:10 +0200

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='csvfilecleaner',
-      version='0.3',
+      version='0.5',
       description='A very simple utility class for cleaning csv files',
       keywords='csv clean file',
       url='',
